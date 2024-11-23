23b2c75563cefa4057d7f41bf7c620507b8c976d
Volodymyr Tymoshenko <vladimir.tymoshenko@branderstudio.com>
2024-11-24 00:15:54 +0100

    dev profiler added

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
